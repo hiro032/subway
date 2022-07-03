49b4e56ea932104d3c46dedebb2d2ae82be9a129
hiro <kiryuinh32@gmail.com>
2022-07-03 11:24:55 +0900

    feat: Line entity

---
+++ src/main/java/hiro/subway/domain/Line.java
@@ -0,0 +1,30 @@
+package hiro.subway.domain;
+
+import javax.persistence.Embedded;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Entity
+public class Line {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String color;
+
+    @Embedded
+    private Sections sections = new Sections();
+
+
+}
